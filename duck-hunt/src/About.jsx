dbf30ca812ee1817a032ae24878a2256bb8940da
AlexDoblyno <alexdoblyno@gmail.com>
2025-12-10 02:47:37 -0700

    更改About github信息。

--- duck-hunt/src/About.jsx
+++ duck-hunt/src/About.jsx
@@ -12,6 +12,7 @@ export default function About() {
         Click on the ducks as they fly across the screen to score points.
       </p>
       <p style={{marginTop: '10px', fontSize: '0.9rem', color: '#666'}}>
+          Built for CS 260.
          Find source code on https://github.com/AlexDoblyno/
           CS260_huntDuck_demo4school.git
       </p>
